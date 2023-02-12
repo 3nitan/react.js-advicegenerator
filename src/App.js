d624009f02918ebaa288d7c5e376476dcc694da1
Abimbola <mustaphaabimbola4@gmail.com>
2023-02-12 11:40:04 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import mobileDivider from './images/pattern-divider-mobile.svg';
+import desktopDivider from './images/pattern-divider-desktop.svg';
+import dice from './images/icon-dice.svg';
 
 function App() {
+  const [text, setText] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+
+  const fetchAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+
+    console.log(data);
+
+    setText(data);
+  };
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <h1>advice #{text.slip.id}</h1>
+      <p>{text.slip.advice}</p>
+
+      <picture>
+        <source media='(min-width: 768px)' srcSet={desktopDivider} />
+        <img src={mobileDivider} alt='' />
+      </picture>
+
+      <div>
+        <button onClick={fetchAdvice}>
+          <img src={dice} alt='' />
+        </button>
+      </div>
     </div>
   );
 }
-
 export default App;
